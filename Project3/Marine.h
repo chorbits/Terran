866646a5bb0607630fab2bfa4d7aa89d267f0df7
odysseyoh <odysseyoh@yahoo.com>
2016-12-01 21:58:37 +0900

    Marine
    Marine

---
+++ Project3/Marine.h
@@ -0,0 +1,21 @@
+#ifndef Marine_h
+#define Marine_h
+
+#include <string>
+#include "Terran.h"
+
+using namespace std;
+
+class Marine : public Terran {
+public:
+	
+	Marine(string name);
+	~Marine();
+
+	virtual void move();
+	virtual void attack();
+
+	void steampack();
+};
+
+#endif
