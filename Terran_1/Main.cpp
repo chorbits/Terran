414e349f242c4c008eb9a5d3d770376d34f9697a
chorbits <chorbits@naver.com>
2016-12-01 22:00:51 +0900

    Update Main.cpp

--- Terran_1/Main.cpp
+++ Terran_1/Main.cpp
@@ -1,13 +1,79 @@
 #include <iostream>
-#include "Main.h"
-
+#include <string>
+ 
+#include "Marine.h"
+#include "Tank.h"
+#include "Terran.h"
+#include "Medic.h"
+ 
+#define MAX_UNIT_COUNT 5
+ 
+using namespace std;
+ 
 int main() {
-	std::cout << "Hello World" << std::endl;
-	getchar();
-	return 0;
-}
-
-int Main::main()
-{
-	return 0;
+     
+    Terran *m[MAX_UNIT_COUNT] = { NULL };
+ 
+    int choice;
+    string name;
+ 
+    //반복문으로 제어: 유닛 생성
+    for (int i = 0; i < MAX_UNIT_COUNT; i++) {
+        cout << "생산할 유닛을 선택하세요 (1=마린, 2=탱크, 3=메딕) : ";
+        cin >> choice;
+        getchar();
+ 
+        cout << "유닛의 이름을 입력하세요: ";
+        cin >> name;
+        getchar();
+ 
+        for (int j = 0; j < i; j++) {
+            while (name.compare(m[j]->getName()) == 0) {
+                cout << "이미 사용 중인 이름입니다. 다른 이름을 입력해주십시오: ";
+                cin >> name;
+            }
+        }
+ 
+        if (choice == 1) {
+            m[i] = new Marine(name);
+        }
+        else if (choice == 2) {
+            m[i] = new Tank(name);  
+        }
+        else if (choice == 3) {
+            m[i] = new Medic(name);
+        }
+    }
+ 
+    cout << "-------------------------------" << endl;
+ 
+    //반복문 제어: 스킬 사용
+    for (int i = 0; i < MAX_UNIT_COUNT; i++) {
+        m[i]->move();
+        m[i]->attack();
+ 
+        Marine *temp1 = dynamic_cast<Marine*>(m[i]);
+        if (temp1 != NULL) {
+            temp1->steampack();
+        }
+ 
+        Tank *temp2 = dynamic_cast<Tank*>(m[i]);
+        if (temp2 != NULL) {
+            temp2->siegeMode();
+        }
+ 
+        Medic *temp3 = dynamic_cast<Medic*>(m[i]);
+        if (temp3 != NULL) {
+            temp3->restore();
+        }
+ 
+    }
+ 
+    for (int i = 0; i < MAX_UNIT_COUNT; i++) {
+        delete m[i];
+    }
+ 
+    getchar();
+    getchar();
+    return 0;
 }
